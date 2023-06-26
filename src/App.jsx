16b3f0956d100a33135237d41c18b347850f60e0
almahmudsarker <almahmuds427@gmail.com>
2023-06-27 02:45:41 +0600

    skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import About from './components/About/About'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import Skills from './components/skills/Skills'
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className="main">
         <Home />
         <About />
+        <Skills />
       </main>
     </>
   )
