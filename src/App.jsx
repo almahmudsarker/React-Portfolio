73eb779a2c62df709025068b5c43de18c84ec033
almahmudsarker <almahmuds427@gmail.com>
2023-06-27 02:06:45 +0600

    about section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import About from './components/About/About'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   )
