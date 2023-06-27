a9354243bcdfe8d8150b5b45afeb99294fe5b017
almahmudsarker <almahmuds427@gmail.com>
2023-06-28 02:08:15 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Skills from './components/skills/Skills'
@@ -15,6 +16,7 @@ function App() {
         <About />
         <Skills />
         <Contact />
+        <Footer />
       </main>
     </>
   )
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,57 @@ import React from 'react';
 import './Footer.css';
 const Footer = () => {
     return (
-        <div>
-            This is footer
+      <footer className="footer">
+        <div className="footer__container container">
+          <h1 className="footer__title">Mahmud</h1>
+
+          <ul className="footer__list">
+            <li>
+              <a href="#about" className="footer__link">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#project" className="footer__link">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="footer__link">
+                Contact
+              </a>
+            </li>
+          </ul>
+
+          <div className="footer__social">
+            <a
+              href="https://www.facebook.com/almahmudsarkerbd/"
+              className="footer__social-link"
+              target="_blank"
+            >
+              <i className="bx bxl-facebook"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/almahmudsarker7/"
+              className="footer__social-link"
+              target="_blank"
+            >
+              <i className="bx bxl-instagram"></i>
+            </a>
+            <a
+              href="https://twitter.com/almahmudsarker7"
+              className="footer__social-link"
+              target="_blank"
+            >
+              <i className="bx bxl-twitter"></i>
+            </a>
+          </div>
+
+          <span className="footer__copy">
+            &#169; Al Mahmud Sarker. All rigths reserved
+          </span>
         </div>
+      </footer>
     );
 };
 
