08998e12d5f8dfc472cc366bd52c0df8d3e1aed2
almahmudsarker <almahmuds427@gmail.com>
2023-06-28 00:11:18 +0600

    contact added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Skills from './components/skills/Skills'
@@ -13,6 +14,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Contact />
       </main>
     </>
   )
