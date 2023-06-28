a0cec29da6f46c85ab93ad18408124ef0b4cafae
almahmudsarker <almahmuds427@gmail.com>
2023-06-28 18:11:39 +0600

    projects added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import Projects from './components/Projects/Projects'
 import ScrollUp from './components/ScrollUp/ScrollUp'
 import Skills from './components/skills/Skills'
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Projects />
         <Contact />
       </main>
         <Footer />
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+      <div>
+        <h2 className="section__title">Projects</h2>
+        <span className="section__subtitle">My Work</span>
+      </div>
+    );
+};
+
+export default Projects;
