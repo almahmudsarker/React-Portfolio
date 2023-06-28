02ef29fb8e1133f9ee847bedbf5e64048a1dfafb
almahmudsarker <almahmuds427@gmail.com>
2023-06-28 15:03:25 +0600

    particle  added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,20 +3,24 @@ import './Home.css';
 import Social from './Social';
 import Data from './Data';
 import ScrollDown from './ScrollDown';
+import Particle from '../Particle';
 const Home = () => {
     return (
-       <section className="home section" id="home">
-              <div className="home__container container grid">
-                <div className="home__content grid">
-                  <Social />
+      <>
+      <Particle />
+        <section className="home section" id="home">
+          <div className="home__container container grid">
+            <div className="home__content grid">
+              <Social />
 
-                  <div className="home__img"></div>
+              <div className="home__img"></div>
 
-                  <Data />  
-                </div>
-                <ScrollDown />
-              </div>
-       </section>
+              <Data />
+            </div>
+            <ScrollDown />
+          </div>
+        </section>
+      </>
     );
 };
 
